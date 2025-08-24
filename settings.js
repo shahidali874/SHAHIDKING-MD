f6a1aed45f86fd43aaf2d804ef927c2aba2cbcb1
shahidali874 <shahidlove78678600@gmail.com>
2025-08-24 22:31:14 +0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: '✨SHAHID KING-MD✨',
+  author: '👑SHAHID KING🇵🇰',
+  botName: '🤖 SHAHID KING-MD',
+  botOwner: 'SHAHID KING-MD', // Your name here u can change 
+  ownerNumber: '923423706291', // Set your number here without + symbol.
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: 'public', // can be 'public' or 'private'
+  description: '💬 A powerful fun WhatsApp MD bot built with ❤️ by shahid king power.',
+  version: '2.0.0',
+};
+
+module.exports = settings;
